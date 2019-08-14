45118afc2f6407088994ce7a65fff6de5b873448
Engkos <engkoskostaman97@gmail.com>
2019-08-14 13:58:34 +0700

    tugas 26 js
    tuhgas ini kami kumpulkan untuk niomic

---
+++ tugas26.js/index.js
@@ -0,0 +1,84 @@
+function panggilisfinite(){
+  var a= isFinite([2,39,76,50,9,7,41,2,24,1,16]);
+  var a = 2;
+   if(a === 2){
+    console.log("Angka "+(a)+" NOT Infinity.")
+  }else{
+    console.log("Angka "+(a)+" Infinity.")
+  }
+  var a = 39;
+   if(a === 39){
+    console.log("Angka "+(a)+" NOT Infinity.")
+  }else{
+    console.log("Angka "+(a)+" Infinity.")
+  }
+  var a = 76;
+   if(a === 76){
+    console.log("Angka "+(a)+" NOT Infinity.")
+  }else{
+    console.log("Angka "+(a)+" Infinity.")
+  }
+  var a = 76;
+   if(a === 76){
+    console.log("Angka "+(a)+" NOT Infinity.")
+  }else{
+    console.log("Angka "+(a)+" Infinity.")
+  }
+  var a = 50;
+   if(a === 50){
+    console.log("Angka "+(a)+" NOT Infinity.")
+  }else{
+    console.log("Angka "+(a)+" Infinity.")
+  }
+  var a = 9;
+   if(a === 9){
+    console.log("Angka "+(a)+" NOT Infinity.")
+  }else{
+    console.log("Angka "+(a)+" Infinity.")
+
+  }
+  var a = 7;
+   if(a === 7){
+    console.log("Angka "+(a)+" NOT Infinity.")
+  }else{
+    console.log("Angka "+(a)+" Infinity.")
+
+  }
+  var a = 41;
+   if(a === 41){
+    console.log("Angka "+(a)+" NOT Infinity.")
+  }else{
+    console.log("Angka "+(a)+" Infinity.")
+
+  }
+  var a = 2;
+   if(a === 2){
+    console.log("Angka "+(a)+" NOT Infinity.")
+  }else{
+    console.log("Angka "+(a)+" Infinity.")
+
+  }
+  var a = 24 ;
+   if(a === 24){
+    console.log("Angka "+(a)+" NOT Infinity.")
+  }else{
+    console.log("Angka "+(a)+" Infinity.")
+
+  }
+  var a = 1;
+   if(a === 1){
+    console.log("Angka "+(a)+" NOT Infinity.")
+  }else{
+    console.log("Angka "+(a)+" Infinity.")
+
+  }
+  var a = 16;
+   if(a === 16){
+    console.log("Angka "+(a)+" NOT Infinity.")
+  }else{
+    console.log("Angka "+(a)+" Infinity.")
+
+  }
+
+}
+panggilisfinite();
